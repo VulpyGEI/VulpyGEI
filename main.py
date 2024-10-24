0cc3ee632a35447605cc2e86f24c2f53f5dfe878
VulpyGEI <138367940+VulpyGEI@users.noreply.github.com>
2024-10-24 13:44:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+# Инициализация переменных
+inputValue = ''
+maxValue = 0
+maxInputValue = ''
+
+while inputValue != '0':
+    inputValue = input('Введите число: ')
+
+    if inputValue != '0':
+        localValue = 0
+
+        for item in inputValue:
+            localValue += int(item)
+
+        # Проверка, является ли текущая сумма цифр больше, чем максимальная
+        if localValue > maxValue:
+            maxValue = localValue
+            maxInputValue = inputValue
+
+print(f'Число с максимальной суммой цифр: {maxInputValue}')
